e81cd005e77188c819eef15c1a54bf09d5c720f4
Matt Huntington <matt.huntington@gmail.com>
2020-06-24 12:34:11 -0400

    copy file with orginal name if no exif exists

--- index.js
+++ index.js
@@ -10,12 +10,25 @@ fs.readdir(dir, (err, files) => {
                 if (error){
                     // console.log('Error: '+error.message);
                     // console.log(file);
+                    fs.copyFile(dir+file, '/Users/matthuntington/Downloads/reorg/'+file, fs.constants.COPYFILE_EXCL, (err) => {
+                        if(err){
+                            console.log(err);
+                        }
+                    });
                 }
                 else{
                     if(exifData.exif.CreateDate){
                         const extension = file.substring(file.lastIndexOf('.'));
                         let newFileName = exifData.exif.CreateDate + "__" + new Date().getTime() + extension;
                         fs.copyFile(dir+file, '/Users/matthuntington/Downloads/reorg/'+newFileName, fs.constants.COPYFILE_EXCL, (err) => {
+                            if(err){
+                                console.log(err);
+                            }
+                        });
+                    } else {
+                        // console.log(exifData);
+                        // console.log(file);
+                        fs.copyFile(dir+file, '/Users/matthuntington/Downloads/reorg/'+file, fs.constants.COPYFILE_EXCL, (err) => {
                             if(err){
                                 console.log(err);
                             }
